d180175635aefab95a91a355a006bd244f337e55
Abhishek Piedy <abhishek.piedy@gmail.com>
2018-07-31 10:59:09 -0400

    list products on page

--- src/pages/buy/buy.ts
+++ src/pages/buy/buy.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FirebaseListObservable } from 'angularfire2/database';
+
+import { ProductService } from '../../providers/products.service';
 
 @Component({
   selector: 'page-buy',
   templateUrl: 'buy.html'
 })
 export class BuyPage {
+  productList: FirebaseListObservable<any[]>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public prodService: ProductService) {
+    this.productList = this.prodService.getProducts();
+  }
 
+  public showProductPage(product) {
+    console.log(product)
   }
 
 }
