f07389e3872d68b5667f7567b4483581f9e0e7ae
Abhishek Piedy <abhishek.piedy@gmail.com>
2018-07-31 14:24:27 -0400

    add functionality to manage listings

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -1,18 +1,60 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { ProductService } from '../../providers/products.service';
+
+import { ProductListing } from '../../models/productListing';
+import { CONST } from '../../shared/constants';
 
 @Component({
   selector: 'page-product',
   templateUrl: 'product.html',
 })
 export class ProductPage {
-  product;
-  mode;
+  public product;
+  public showBuyContent: boolean;
+  public showEditContent: boolean;
+  public editForm: FormGroup;
+  public buyForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private mode: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, public prodService: ProductService) {
     this.product = this.navParams.get('product');
     this.mode = this.navParams.get('mode');
-    console.log("prod", this.product)
-    console.log("mode", this.mode)
+    this.showBuyContent = (this.mode === CONST.buy);
+    this.showEditContent = (this.mode === CONST.edit);
+    this.createForm();
+  }
+
+  private createForm() {
+    if(this.showEditContent) {
+      this.editForm = this.formBuilder.group({
+        name: [this.product.name, Validators.required],
+        price: [this.product.price, Validators.required],
+        quantity: [this.product.number, Validators.required]
+      });
+    }
+    else if(this.showBuyContent) {
+      this.buyForm = this.formBuilder.group({
+        quantity: ['', Validators.required]
+      });
+    }
+  }
+
+  public edit() {
+    const newProductVal = new ProductListing(
+      this.editForm.value.name,
+      this.editForm.value.price,
+      this.editForm.value.quantity,
+      this.product.seller
+    );
+    this.prodService.updateProduct(newProductVal, this.product.$key);
+  }
+
+  public remove() {
+    this.prodService.removeProduct(this.product);
+    this.navCtrl.pop();
   }
 }
