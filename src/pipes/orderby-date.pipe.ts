f21a3fae364496a79b9b6c7b10ed42c681cd5043
Abhishek Piedy <abhishek.piedy@gmail.com>
2018-08-02 16:20:23 -0400

    sort inventory dates in descending order of purchase date

---
+++ src/pipes/orderby-date.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'orderByDate' })
+export class OrderByDatePipe implements PipeTransform {
+    transform(value, order, dateElement) {
+        console.log(value);
+        console.log(order);
+        console.log(dateElement);
+        if(value !== null && value !== undefined) {
+            if(order === 'desc')
+                return value.sort((a, b) => Date.parse(b[dateElement]) - Date.parse(a[dateElement]));
+            else if(order === 'asc')
+                return value.sort((a, b) => Date.parse(a[dateElement]) - Date.parse(b[dateElement]));
+        }
+    }
+}
