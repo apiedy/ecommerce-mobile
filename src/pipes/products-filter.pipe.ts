1808bd1e67eba73cfae3221cb7e21859b419284e
Abhishek Piedy <abhishek.piedy@gmail.com>
2018-08-02 13:25:13 -0400

    filter product listings to filter out the user's own products appropriately on different pages of the app

--- src/pipes/products-filter.pipe.ts
+++ src/pipes/products-filter.pipe.ts
@@ -4,7 +4,8 @@ import { CONST } from '../shared/constants';
 @Pipe({ name: 'prodFilter' })
 export class ProductsFilter implements PipeTransform {
     transform(value: any[], filter: string) {
-        if(value !== null ) {
+        if(value !== null && value !== undefined) {
+
             if (filter === CONST.buy.toLowerCase())
                 return value.filter(product => product.price !== undefined)
             else if (filter === CONST.trade.toLowerCase())
