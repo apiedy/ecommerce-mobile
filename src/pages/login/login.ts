0ffc76f8a7fd6c0a4011461023c0f2b7da481f7a
Abhishek Piedy <abhishek.piedy@gmail.com>
2018-08-02 17:02:49 -0400

    add login toast

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, LoadingController } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, Toast } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { UserProvider } from '../../providers/user.service';
+import { ToastService } from '../../providers/toast.service';
 
 import { SignupPage } from '../signup/signup';
 import { TabsPage } from '../tabs/tabs';
@@ -21,7 +22,8 @@ export class LoginPage {
               public navParams: NavParams,
               private formBuilder: FormBuilder,
               public userService: UserProvider,
-              public loadingCtrl: LoadingController) {
+              public loadingCtrl: LoadingController,
+              private toastService: ToastService) {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
@@ -36,19 +38,23 @@ export class LoginPage {
 
   public login() {
     this.loading.present();
-
+    
     const username = this.loginForm.value.username;
     const password = this.loginForm.value.password;
-
+    
     this.userService.login(username, password).subscribe(
       (user) => {
         if(user[0]) {
+          const toast: Toast = this.toastService.createToast(`Welcome, ${username}!`, 'bottom', 'Ok');
+          this.toastService.openToast(toast, true);
           this.userService.setCurrentUser(user[0]);
           this.loading.dismiss();
           this.navCtrl.setRoot(TabsPage);
         }
       },
-      (err) => { },
+      (err) => {
+        this.loading.dismiss();
+      }
     )
   }
 }
